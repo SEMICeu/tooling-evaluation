0ab90ea73796f9a476db66e91668c163eb0c6fe6
EmidioStani <emidiostani@gmail.com>
2025-05-14 12:37:40 +0200

    updating model

--- besser/core-person.py
+++ besser/core-person.py
@@ -1,42 +1,36 @@
 from besser.BUML.metamodel.structural import DomainModel, Class, Property, \
-    Multiplicity, BinaryAssociation, StringType, IntegerType, DateType
+    Multiplicity, StringType, IntegerType, DateType, Association, BinaryAssociation
 from besser.generators.rdf import RDFGenerator
 
-# Library attributes definition
-library_name: Property = Property(name="name", type=StringType)
-address: Property = Property(name="address", type=StringType)
+# Person attributes definition
+person_birthName: Property = Property(name="birthName", type=StringType)
+person_birthDate: Property = Property(name="birthDate", type=DateType)
+person_deathDate: Property = Property(name="deathDate", type=DateType)
+person_familyName: Property = Property(name="familyName", type=StringType)
+person_fullName: Property = Property(name="fullName", type=StringType)
+person_givenName: Property = Property(name="givenName", type=StringType)
 # Library class definition
-library: Class = Class(name="Library", attributes={library_name, address})
+person: Class = Class(name="Person", attributes={person_birthName, person_birthDate,
+                                                person_deathDate, person_familyName,
+                                                person_fullName, person_givenName})
 
-# Book attributes definition
-title: Property = Property(name="title", type=StringType)
-pages: Property = Property(name="pages", type=IntegerType)
-release: Property = Property(name="release", type=DateType)
-# Book class definition
-book: Class = Class(name="Book", attributes={title, pages, release})
+# Address attributes definition
+address_postName: Property = Property(name="postName", type=StringType)
+address_postCode: Property = Property(name="postCode", type=IntegerType)
+# Address class definition
+address: Class = Class(name="Address", attributes={address_postName, address_postCode})
 
-# Author attributes definition
-author_name: Property = Property(name="name", type=StringType)
-email: Property = Property(name="email", type=StringType)
-# Author class definition
-author: Class = Class(name="Author", attributes={author_name, email})
-
-# Library-Book association definition
-located_in: Property = Property(name="locatedIn", type=library, multiplicity=Multiplicity(1, 1))
-has: Property = Property(name="has", type=book, multiplicity=Multiplicity(0, "*"))
-lib_book_association: BinaryAssociation = BinaryAssociation(name="lib_book_assoc", ends={located_in, has})
-
-# Book-Author association definition
-publishes: Property = Property(name="publishes", type=book, multiplicity=Multiplicity(0, "*"))
-written_by: Property = Property(name="writtenBy", type=author, multiplicity=Multiplicity(1, "*"))
-book_author_association: BinaryAssociation = BinaryAssociation(name="book_author_assoc", ends={written_by, publishes})
+# Person-Address association definition
+person_address_domicile: Property = Property(name="domicile", type=address, multiplicity=Multiplicity(0, "*"))
+address_person_has: Property = Property(name="has", owner=None, type=person, multiplicity=Multiplicity(0, "*"))
+person_address_association: Association = Association(name="domicile", ends={person_address_domicile, address_person_has})
 
 # Domain model definition
-library_model: DomainModel = DomainModel(name="Library_model", types={library, book, author},
-                                         associations={lib_book_association, book_author_association})
+library_model: DomainModel = DomainModel(name="Library_model", types={person, address},
+                                         associations={person_address_association})
 
-# Getting the attributes of the Book class
-for attribute in book.attributes:
+# Getting the attributes of the Person class
+for attribute in person.attributes:
     print(attribute.name)
 
 # Code Generation
